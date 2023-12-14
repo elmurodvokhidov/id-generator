6c704b5dd3abfcd4b6aac91047afc015d812a77c
elmurod <elmurodvkh@gmail.com>
2023-12-15 00:24:26 +0500

    updated minor version

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 function getUID() {
     let abc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
-    let x = Math.floor(Math.random() * 999);
-    let y = [abc[Math.floor(Math.random() * abc.length)], abc[Math.floor(Math.random() * abc.length)]].join('');
+    let x = Math.floor(Math.random() * 9999);
+    let y = '';
+    for (let i = 0; i < 3; i++) {
+        y += abc[Math.floor(Math.random() * abc.length)];
+    }
     let z = [new Date().getSeconds(), new Date().getMilliseconds()].join('');
 
-    return x + y + z;
+    return btoa(x + y + z) + x + y + z;
 };
 
 export default getUID;
